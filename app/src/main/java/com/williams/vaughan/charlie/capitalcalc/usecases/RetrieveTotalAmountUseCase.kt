03d1e7a66976dc6b492185c735a4cb7b826cb80a
charliewilliams <charliewilliamsweb@gmail.com>
2020-06-21 18:36:14 +0100

    Merge branch 'develop' of C:\Users\cvwbu\Google Drive\CapitalCalc with conflicts.

---
+++ app/src/main/java/com/williams/vaughan/charlie/capitalcalc/usecases/RetrieveTotalAmountUseCase.kt
@@ -0,0 +1,90 @@
+package com.williams.vaughan.charlie.capitalcalc.usecases
+
+import com.williams.vaughan.charlie.capitalcalc.CompoundInterval.ANNUALLY
+import com.williams.vaughan.charlie.capitalcalc.CompoundInterval.BIANNUALLY
+import com.williams.vaughan.charlie.capitalcalc.CompoundInterval.DAILY
+import com.williams.vaughan.charlie.capitalcalc.CompoundInterval.MONTHLY
+import com.williams.vaughan.charlie.capitalcalc.CompoundInterval.QUARTERLY
+import java.math.BigDecimal
+import java.math.MathContext
+
+class RetrieveTotalAmountUseCase(
+    private val convertStringsToUsableUseCase: ConvertStringsToUsableUseCase
+) {
+
+    fun execute(retrieveTotalAmountUseCaseParams: RetrieveTotalAmountUseCaseParams): UseCaseResults {
+        val results = convertStringsToUsableUseCase.execute(retrieveTotalAmountUseCaseParams)
+        with(results) {
+            return if (principalAmount == null ||
+                annualInterestRate == null ||
+                calculationPeriod == null ||
+                depositAmount == null
+            ) {
+                UseCaseResults(false, "", "")
+            } else if (monthlyDeposits) {
+                performMonthlyDepositCalculation(results)
+            } else {
+                performNonMonthlyDepositCalculation(results)
+            }
+        }
+    }
+
+    // Compound interest formulae: https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php
+    private fun performNonMonthlyDepositCalculation(results: UsableUseCaseParams): UseCaseResults {
+        with(results) {
+            val p = principalAmount
+            val r = annualInterestRate
+            val n = BigDecimal(0)
+            val t = calculationPeriod
+
+            val brackets = BigDecimal(1).add(r?.divide(n, MathContext.DECIMAL128))
+            val upper = t?.let { n.times(it) }
+            val main = upper?.let { brackets.times(it) }
+            val amount = main?.let { p?.times(it) }
+
+            return UseCaseResults(true, amount.toString(), calculationPeriod.toString())
+        }
+    }
+
+    private fun performMonthlyDepositCalculation(results: UsableUseCaseParams): UseCaseResults {
+        with(results) {
+            return when (depositAmount == null) {
+                true -> UseCaseResults(false, "", "")
+                false -> {
+                    val p = principalAmount
+                    val r = annualInterestRate
+                    val n = when (compoundInterval) {
+                        DAILY -> BigDecimal(365)
+                        MONTHLY -> BigDecimal(12)
+                        QUARTERLY -> BigDecimal(4)
+                        BIANNUALLY -> BigDecimal(2)
+                        ANNUALLY -> BigDecimal(1)
+                    }
+                    val t = calculationPeriod
+
+                    val brackets = BigDecimal(1).add(r?.divide(n, MathContext.DECIMAL128))
+                    val upper = t?.let { n.times(it) }
+                    val main = upper?.let { brackets.times(it) }
+                    val amount = main?.let { p?.times(it) }
+
+                    return UseCaseResults(true, amount.toString(), calculationPeriod.toString())
+                }
+            }
+        }
+    }
+}
+
+data class RetrieveTotalAmountUseCaseParams(
+    val principalAmount: String,
+    val annualInterestRate: String,
+    val calculationPeriod: String,
+    val compoundInterval: Int,
+    val monthlyDeposits: Boolean,
+    val depositAmount: String?
+)
+
+data class UseCaseResults(
+    val success: Boolean,
+    val totalAmount: String,
+    val timePeriod: String
+)
--- app/src/main/java/com/williams/vaughan/charlie/capitalcalc/viewmodels/CalculatorViewModel.kt
+++ app/src/main/java/com/williams/vaughan/charlie/capitalcalc/viewmodels/CalculatorViewModel.kt
@@ -3,29 +3,90 @@ package com.williams.vaughan.charlie.capitalcalc.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.williams.vaughan.charlie.capitalcalc.extensions.Event
+import com.williams.vaughan.charlie.capitalcalc.usecases.RetrieveTotalAmountUseCase
+import com.williams.vaughan.charlie.capitalcalc.usecases.RetrieveTotalAmountUseCaseParams
+import com.williams.vaughan.charlie.capitalcalc.viewstates.CalculatorNavigationEffect
+import com.williams.vaughan.charlie.capitalcalc.viewstates.CalculatorNavigationEffect.NavigateToResultEffect
+import com.williams.vaughan.charlie.capitalcalc.viewstates.CalculatorViewEffect
+import com.williams.vaughan.charlie.capitalcalc.viewstates.CalculatorViewEffect.ShowToastEffect
 import com.williams.vaughan.charlie.capitalcalc.viewstates.CalculatorViewEvent
+import com.williams.vaughan.charlie.capitalcalc.viewstates.CalculatorViewEvent.CalculateButtonPressed
 import com.williams.vaughan.charlie.capitalcalc.viewstates.CalculatorViewEvent.ScreenLoadEvent
 import com.williams.vaughan.charlie.capitalcalc.viewstates.CalculatorViewState
+import kotlinx.coroutines.runBlocking
 
-class CalculatorViewModel : ViewModel() {
+class CalculatorViewModel(
+    private val retrieveTotalAmountUseCase: RetrieveTotalAmountUseCase
+) : ViewModel() {
 
     private val viewState: MutableLiveData<CalculatorViewState> = MutableLiveData()
+    private val viewEffect = MutableLiveData<Event<CalculatorViewEffect>>()
+    private val navigationEffect = MutableLiveData<Event<CalculatorNavigationEffect>>()
 
     fun onEvent(event: CalculatorViewEvent) {
         when (event) {
             is ScreenLoadEvent -> onScreenLoad()
+            is CalculateButtonPressed -> with(event) {
+                onCalculateButtonPressed(
+                    principalAmount,
+                    annualInterestRate,
+                    calculationPeriod,
+                    compoundInterval,
+                    monthlyDeposits,
+                    depositAmount
+                )
+            }
         }
     }
 
     private fun onScreenLoad() {
-
-    }
-
-    private fun onScreenLoadSuccess() {
         viewState.value = CalculatorViewState(
-            isLoading = false
+            principalAmount = "",
+            annualInterestRate = "",
+            calculationPeriod = "",
+            compoundInterval = 2,
+            monthlyDeposits = false,
+            depositAmount = ""
         )
     }
 
+    private fun onCalculateButtonPressed(
+        principalAmount: String,
+        annualInterestRate: String,
+        calculationPeriod: String,
+        compoundInterval: Int,
+        monthlyDeposits: Boolean,
+        depositAmount: String
+    ) {
+        viewState.value = viewState.value?.copy(
+            principalAmount = principalAmount,
+            annualInterestRate = annualInterestRate,
+            calculationPeriod = calculationPeriod,
+            compoundInterval = compoundInterval,
+            monthlyDeposits = monthlyDeposits,
+            depositAmount = depositAmount
+        )
+
+        val results = runBlocking {
+            retrieveTotalAmountUseCase.execute(
+                RetrieveTotalAmountUseCaseParams(
+                    principalAmount,
+                    annualInterestRate,
+                    calculationPeriod,
+                    compoundInterval,
+                    monthlyDeposits,
+                    depositAmount
+                )
+            )
+        }
+        when (results.success) {
+            true -> navigationEffect.value = Event(NavigateToResultEffect)
+            false -> viewEffect.value = Event(ShowToastEffect)
+        }
+    }
+
     fun viewState(): LiveData<CalculatorViewState> = viewState
+    fun getViewEffect(): LiveData<Event<CalculatorViewEffect>> = viewEffect
+    fun getNavigationEffect(): LiveData<Event<CalculatorNavigationEffect>> = navigationEffect
 }
