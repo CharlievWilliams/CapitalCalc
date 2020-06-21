508ea4aebc69ee415f636377dbb7fb32d92e6e9a
charliewilliams <charliewilliamsweb@gmail.com>
2020-06-21 02:07:47 +0100

    #1 Refactor fragment_calculator.xml
    * Simplify ViewStates
    * Rename all XML values

--- app/src/main/java/com/williams/vaughan/charlie/capitalcalc/viewstates/CalculatorViewState.kt
+++ app/src/main/java/com/williams/vaughan/charlie/capitalcalc/viewstates/CalculatorViewState.kt
@@ -1,12 +1,8 @@
 package com.williams.vaughan.charlie.capitalcalc.viewstates
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class CalculatorViewState(
     val isLoading: Boolean = true
-) : Parcelable
+)
 
 sealed class CalculatorViewEvent {
     object ScreenLoadEvent : CalculatorViewEvent()
